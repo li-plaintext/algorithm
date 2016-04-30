188783029cd7c40cb7683099c8f24ffa5ec45cdc
li.xu <li.xu@theoddsbroker.com>
2016-04-30 13:43:45 +1000

    mergeSort

--- mergSort/mergeSort.js
+++ mergSort/mergeSort.js
@@ -1,35 +1,28 @@
-        function mSort(arr){
-            var mid = Math.floor(arr.length / 2);
+function mSort(arr){
 
-            if(mid > 0){
-                var arrL = mSort(arr.slice(0,mid));
-                var arrR = mSort(arr.slice(mid,arr.length));
-                return merge(arrL, arrR);
-            }else{
-                return arr;
-            }
+    if(arr.length < 2){
+        return arr;
+    }
 
+    var mid = Math.floor(arr.length / 2);
+    var arrL = mSort(arr.slice(0,mid));
+    var arrR = mSort(arr.slice(mid));
+    return merge(arrL, arrR);
+}
+function merge(arrL, arrR){
+    var idxL = 0;
+    var idxR = 0;
+    var res = [];
+    while(idxL < arrL.length && idxR < arrR.length){
+        if(arrL[idxL] <= arrR[idxR]){
+            res.push(arrL[idxL]);
+            idxL++;
+        }else{
+            res.push(arrR[idxR]);
+            idxR++;
         }
-        function merge(arrL, arrR){
-            var idxL = 0;
-            var idxR = 0;
-            var res = [];
-            while(idxL < arrL.length && idxR < arrR.length){
-                if(arrL[idxL] <= arrR[idxR]){
-                    res.push(arrL[idxL]);
-                    idxL++;
-                }else{
-                    res.push(arrR[idxR]);
-                    idxR++;
-                }
-            }
-            if(idxL < arrL.length){
-                res = res.concat(arrL.slice(idxL));
-            }
-            if(idxR < arrR.length){
-                res = res.concat(arrR.slice(idxR));
-            }
-            return res;
-        }
+    }
+    return res.concat(arrL.slice(idxL), arrR.slice(idxR));
+}
 
 
